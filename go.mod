5d8877576657e0544014669a6de16eae60506b4d
Ox Cart <ox.to.a.cart@gmail.com>
2021-08-11 08:03:21 -0500

    Always use NAT64 if available

--- go.mod
+++ go.mod
@@ -3,14 +3,12 @@ module github.com/getlantern/netx
 go 1.14
 
 require (
-	github.com/getlantern/context v0.0.0-20190109183933-c447772a6520 // indirect
 	github.com/getlantern/errors v1.0.1
 	github.com/getlantern/fdcount v0.0.0-20190912142506-f89afd7367c4
-	github.com/getlantern/hex v0.0.0-20190417191902-c6586a6fe0b7 // indirect
-	github.com/getlantern/hidden v0.0.0-20190325191715-f02dbb02be55 // indirect
+	github.com/getlantern/golog v0.0.0-20210606115803-bce9f9fe5a5f
+	github.com/getlantern/iptool v0.0.0-20210721034953-519bf8ce0147 // indirect
 	github.com/getlantern/mockconn v0.0.0-20200818071412-cb30d065a848
 	github.com/getlantern/mtime v0.0.0-20200417132445-23682092d1f7 // indirect
 	github.com/getlantern/ops v0.0.0-20200403153110-8476b16edcd6 // indirect
-	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
 )
--- netx.go
+++ netx.go
@@ -3,59 +3,102 @@
 package netx
 
 import (
+	"bytes"
 	"context"
 	"net"
-	"strings"
+	"sync"
 	"sync/atomic"
 	"time"
+
+	"github.com/getlantern/golog"
+	"github.com/getlantern/iptool"
 )
 
 var (
-	dial               atomic.Value
-	dialUDP            atomic.Value
-	listenUDP          atomic.Value
-	resolveTCPAddr     atomic.Value
-	resolveUDPAddr     atomic.Value
-	NAT64Prefix        atomic.Value
-	defaultDialTimeout = 1 * time.Minute
+	log = golog.LoggerFor("netx")
+)
+
+var (
+	dial                  atomic.Value
+	dialUDP               atomic.Value
+	listenUDP             atomic.Value
+	resolveTCPAddr        atomic.Value
+	resolveUDPAddr        atomic.Value
+	enableNAT64Once       sync.Once
+	nat64Prefix           []byte
+	nat64PrefixMx         sync.RWMutex
+	updateNAT64PrefixCh   = make(chan interface{}, 1)
+	defaultDialTimeout    = 1 * time.Minute
+	minNAT64QueryInterval = 10 * time.Second
+	zero                  = []byte{0}
+	ipt                   iptool.Tool
 )
 
 func init() {
+	ipt, _ = iptool.New()
 	Reset()
 }
 
-type NAT64PrefixHolder struct {
-	expiration time.Time
-	prefix     []byte
+// EnableNAT64 enables automatic discovery of NAT64 prefix using DNS query for ipv4only.arpa.
+// Once enabled, netx will automatically dial IPv4 addresses via IPv6 using this prefix
+// if it is available
+func EnableNAT64AutoDiscovery() {
+	enableNAT64Once.Do(func() {
+		log.Debug("Enabling NAT64 auto-discovery")
+		go func() {
+			var priorNAT64Prefix []byte
+			for {
+				log.Debugf("Checking for updated NAT64 prefix")
+				updateNAT64Prefix()
+				nextNAT64Prefix := getNAT64Prefix()
+				if !bytes.Equal(priorNAT64Prefix, nextNAT64Prefix) {
+					log.Debugf("NAT64 prefix changed from %v to %v", priorNAT64Prefix, nextNAT64Prefix)
+					priorNAT64Prefix = nextNAT64Prefix
+				}
+				// Don't updat NAT64 prefix too often
+				time.Sleep(minNAT64QueryInterval)
+				// Only update NAT64 Prefix again if it's necessary
+				<-updateNAT64PrefixCh
+			}
+		}()
+	})
 }
 
-func ResetNAT64Prefix() {
-	NAT64Prefix.Store(nil)
-}
-
-// getNAT64Prefix returns previously fetched ipv6 prefix, or gets a fresh one using DNS lookup
-func getNAT64Prefix() []byte {
-	if holder, ok := NAT64Prefix.Load().(*NAT64PrefixHolder); holder != nil && ok {
-		if time.Now().Before(holder.expiration) {
-			return holder.prefix
-		}
-	}
+func updateNAT64Prefix() {
 	ips, err := net.LookupIP("ipv4only.arpa")
 	if err == nil {
 		for _, ip := range ips {
 			if ip.To4() == nil {
 				prefix := ip[:12]
-				NAT64Prefix.Store(&NAT64PrefixHolder{prefix: prefix, expiration: time.Now().Add(time.Hour * 1)})
-				return prefix
+				if bytes.Count(prefix, zero) < 12 {
+					nat64PrefixMx.Lock()
+					nat64Prefix = prefix
+					nat64PrefixMx.Unlock()
+					return
+				}
 			}
 		}
+
+		nat64PrefixMx.Lock()
+		nat64Prefix = nil
+		nat64PrefixMx.Unlock()
 	}
-	return nil
 }
 
-// isNetworkUnreachable checks if the error matches string representation of ENETUNREACH
-func isNetworkUnreachable(err error) bool {
-	return err != nil && strings.Contains(err.Error(), "unreachable")
+func refreshNAT64Prefix() {
+	select {
+	case updateNAT64PrefixCh <- nil:
+		// requested refresh of NAT64 prefx
+	default:
+		// refresh already pending
+	}
+}
+
+// getNAT64Prefix returns previously fetched ipv6 prefix, or gets a fresh one using DNS lookup
+func getNAT64Prefix() []byte {
+	nat64PrefixMx.RLock()
+	defer nat64PrefixMx.RUnlock()
+	return nat64Prefix
 }
 
 // convertAddressDNS64 takes the IP address, converts it to ipv6 and applies DNS64 prefix
@@ -66,6 +109,12 @@ func convertAddressDNS64(addr string) string {
 	}
 	ip := net.ParseIP(host)
 	if ip.To4() == nil { // if it's ipv6 already - don't do anything
+		return addr
+	}
+	if ipt.IsPrivate(&net.IPAddr{
+		IP: ip,
+	}) {
+		// don't mess with private IP addresses
 		return addr
 	}
 	prefix := getNAT64Prefix()
@@ -100,14 +149,17 @@ func DialTimeout(network string, addr string, timeout time.Duration) (net.Conn, 
 // DialContext dials the given addr on the given net type using the configured
 // dial function, with the given context.
 func DialContext(ctx context.Context, network string, addr string) (net.Conn, error) {
+	// always convert IPv4 addresses to use a NAT64 prefix if we're on a NAT64 network
+	// if EnableNAT64Autodiscovery hasn't been called, if addr is an IPv6 address, if
+	// addr is a local address or if we haven't autodiscovered a NAT64 prefix, this is a
+	// no-op.
+	addr = convertAddressDNS64(addr)
 	dialer := dial.Load().(func(context.Context, string, string) (net.Conn, error))
-
 	conn, err := dialer(ctx, network, addr)
-	ipv4Network := network == "udp4" || network == "tcp4"
-	// if we are not dialing an explicitly ipv4 network and we got ENETUNREACH - try applying DNS64 prefix
-	if !ipv4Network && isNetworkUnreachable(err) {
-		addr = convertAddressDNS64(addr)
-		conn, err = dialer(ctx, network, addr)
+	if err != nil {
+		// error might be because we're now on a NAT64 network (or a different NAT64 network)
+		// request a refresh of the NAT64 prefix
+		refreshNAT64Prefix()
 	}
 	return conn, err
 }
